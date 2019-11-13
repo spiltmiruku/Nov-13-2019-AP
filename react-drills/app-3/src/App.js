45005a8f6060dd7efc8c8bd733f422436d0bb2d2
spiltmiruku <kitsunedg@gmail.com>
2019-11-13 15:34:42 -0700

    Finished app-2 & app-3

--- react-drills/app-3/src/App.js
+++ react-drills/app-3/src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      addNew: '',
+      pinkPokemon: ['jigglypuff', 'igglybuff', 'clefairy', 'cleffa', 'slurpuff']
+    };
+  }
+
+handleChange(filter){
+  this.setState({ addNew: filter })
+}
+
   render() {
+    let cutePokemon = this.state.pinkPokemon
+    .filter((element, index) => {
+      return element.includes(this.state.addNew);
+    })
+    .map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    });
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <input
+      onChange={e => this.handleChange(e.target.value)} 
+      type="text"/>
+      {cutePokemon}
+      </div> );
   }
 }
 
