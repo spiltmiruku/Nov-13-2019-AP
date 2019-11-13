fd75dc4d46119102e1a29f4c0d1e8a181dfcc611
spiltmiruku <kitsunedg@gmail.com>
2019-11-13 14:25:02 -0700

    Finished app-1

--- react-drills/app-1/src/App.js
+++ react-drills/app-1/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      message: ""
+    };
+  }
+
+  handleChange(value) {
+    this.setState({ message: value });
+  }
+
   render() {
     return (
       <div className="App">
-
-
+<input 
+onChange={(e) => this.handleChange(e.target.value)}
+type="text"/>
+  <p>{this.state.message}</p>
       </div>
     );
   }
