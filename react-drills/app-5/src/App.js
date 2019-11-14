833c43c109b3bb19aed950df8146592138b8db72
spiltmiruku <kitsunedg@gmail.com>
2019-11-13 20:05:43 -0700

    Finished app-5

--- react-drills/app-5/src/App.js
+++ react-drills/app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+
+import Image from './Image';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App"> 
+      <Image url ={'https://i.pinimg.com/originals/64/a1/3d/64a13dd97aa84fff436f2b60e64211a4.gif'}/>
       </div>
     );
   }
---
+++ react-drills/app-5/src/Image.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+
+    render(){
+        return(
+            <div>
+            <img src="https://66.media.tumblr.com/e24a76ef433f6694aef0f13e17170e86/tumblr_pg7zz1S0iZ1xq1em9o2_500.gifv" alt="large marbles floating in water"/>
+            <div>Large glass marbles floating in water</div>
+            </div>
+    )};
+}
+export default Image;
